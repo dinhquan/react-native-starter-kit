b0923ca2229655844f8974b099141adf39a9f413
Quan <quannd2@smartosc.com>
2021-04-01 16:36:11 +0700

    Remove test

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,3 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    [
-      require.resolve('babel-plugin-module-resolver'),
-      {
-        root: ['./src/'],
-        alias: {
-          core: './src/core',
-          ui: './src/ui',
-        },
-      },
-    ],
-    ['@babel/plugin-proposal-decorators', {legacy: true}],
-  ],
 };
